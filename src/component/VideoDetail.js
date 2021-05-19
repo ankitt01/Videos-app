37c303e4b855a5d3872f24d0718b94a69340a8aa
ankitt01 <ankitrawat0102@gmail.com>
2021-05-19 22:24:26 +0530

    styling VidoeDetail

--- src/component/VideoDetail.js
+++ src/component/VideoDetail.js
@@ -4,7 +4,15 @@ const VideoDetail = ({video}) => {
     if(!video) {
         return <div>Loading...</div>
     }
-    return <div>{video.snippet.title}</div>
+    return (
+        <div>
+            <div>{video.snippet.title}</div>
+            <div className="ui segment">
+                <h4 classNameui header>{video.snippet.title}</h4> 
+                <p>{video.snippet.description}</p>
+            </div>
+        </div>
+        );
 }
 
 export default VideoDetail;
